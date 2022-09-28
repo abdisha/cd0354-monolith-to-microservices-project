38b230272ca8cf19e003cade211b8d579a5bffd9
abdisha <asresabdi@gmail.com>
2022-09-28 09:56:45 +0300

    configured docker

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5ab4a13b0e6042cbba3557999099ba8-1030284092.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
