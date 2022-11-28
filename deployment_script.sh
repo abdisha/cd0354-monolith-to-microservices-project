ed16af4eae8998cd25b9b0c31e108c75e82fc241
abdisha <asresabdi@gmail.com>
2022-11-28 17:55:25 +0300

    port number configured

--- deployment_script.sh
+++ deployment_script.sh
@@ -1,10 +1,15 @@
-kubectl delete deploy backend-user
-kubectl delete deploy backend-feed
+kubectl delete deploy udagram-api-feed
+kubectl delete deploy udagram-api-user
+kubectl delete deploy udagram-frontend
 kubectl delete deploy reverseproxy
-kubectl delete deploy frontend
 
-
+#create or apply deployment
 kubectl apply -f udagram-api-feed/backend-feed-deployment.yaml
 kubectl apply -f udagram-api-user/backend-user-deployment.yaml
 kubectl apply -f udagram-frontend/frontend-deployment.yaml
 kubectl apply -f udagram-reverseproxy/reverseproxy-deployment.yaml
+#create or apply service files
+kubectl apply -f udagram-api-feed/backend-feed-service.yaml
+kubectl apply -f udagram-api-user/backend-user-service.yaml
+kubectl apply -f udagram-frontend/frontend-service.yaml
+kubectl apply -f udagram-reverseproxy/reverseproxy-service.yaml
